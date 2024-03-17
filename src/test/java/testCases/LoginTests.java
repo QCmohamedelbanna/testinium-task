9bebfc48c24eaaa1bb865c3c03f33698e21aa120
Mo.elbanna <60271927+QCmohamedelbanna@users.noreply.github.com>
2024-03-17 23:43:01 +0200

    enhancement in login test case

--- src/test/java/testCases/LoginTests.java
+++ src/test/java/testCases/LoginTests.java
@@ -4,25 +4,20 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pom.HomePage;
 import pom.LoginPage;
-import utils.SeleniumActions;
 import utils.SeleniumBase;
 
-public class SearchTests {
-
+public class LoginTests {
     HomePage homePage = new HomePage();
-
-
+    LoginPage loginPage = new LoginPage();
     @BeforeTest
     public void init() {
         SeleniumBase seleniumBase = new SeleniumBase();
         seleniumBase.seleniumConfig();
         seleniumBase.environmentSetup();
     }
-
     @Test
-    public void checkThatAppRedirectedToTheSearchedText(){
-        homePage.searchForText("Barcelona");
+    public void checkThatUserLoggedInSuccessfully(){
+        homePage.goToLoginPage();
+        loginPage.login("Elbanna22","Test@123");
     }
-
-
 }
